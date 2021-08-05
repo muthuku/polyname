306713c007664649d9644f368658476098a5d9b4
muthuku <67440419+muthuku@users.noreply.github.com>
2021-08-05 13:15:17 -0400

    update

--- polyname/check_database.py
+++ polyname/check_database.py
@@ -66,13 +66,15 @@ def search_names(file, final_names):
 
 def get_cirpy_polymer_names(polymer_name):
     polymer_names = []
-    name = cirpy.resolve(polymer_name, 'iupac_name')
-    if name is None:
+    name = polymer_name.replace("(", "")
+    name = polymer_name.replace(")", "")
+    iupac_name = cirpy.resolve(name, 'iupac_name')
+    if iupac_name is None:
     	polymer_names.append("None")
     else:
     	polymer_names.append(name)
     return polymer_names
-
+'''
 def get_cirpy_polymer_smiles(polymer_name):
     polymer_smiles = []
     smile = cirpy.resolve(polymer_name, 'smiles')
@@ -81,7 +83,7 @@ def get_cirpy_polymer_smiles(polymer_name):
     else:
     	polymer_smiles.append(smile)
     return polymer_smiles
-#example to run the script
+#example to run the script'''
 '''files_path = '/Users/pmuthuku/Documents/python_scripts_suli/workingscripts/polyname_scripts/polymername_final.txt'
 data_base = "polyname_smiles_test.csv"
 
